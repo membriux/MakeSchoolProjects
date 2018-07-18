b4460a1f42212ffefb54c7076758ef0ee840af68
Guillermo Ignacio Sanchez Lamas <sanguillermo98@gmail.com>
2018-07-18 09:51:34 -0700

    Worked on Vocab-Builder; Added Thesaurus API Test file

---
+++ GetThesaurus.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: A UIKit based Playground for presenting user interface
+  
+import Foundation
+
+struct Thesaurus {
+    
+    let word: String
+    let definition: String
+    let wordType: String
+
+    
+
+    
+}
